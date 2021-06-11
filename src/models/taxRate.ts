3e0ca620a4a80776bcadc2bc9acc9da133ebe89c
Derek Milne <derekm@iqmetrix.com>
2021-06-11 14:26:36 -0700

    tax rate model def

---
+++ src/models/taxRate.ts
@@ -0,0 +1,27 @@
+import { Model } from "./model";
+import { extractNumber, extractString, JsonType } from "../json-util";
+
+export class TaxRate extends Model {
+  constructor(
+    readonly id: number,
+    readonly createdAt: Date,
+    readonly updatedAt: Date | null,
+    readonly locationId: number,
+    readonly name: string,
+    readonly rate: number
+  ) {
+    super(id, createdAt, updatedAt);
+  }
+
+  static fromJSON(d: string | JsonType): TaxRate {
+    if (typeof d === "string") {
+      d = JSON.parse(d) as JsonType;
+    }
+    return new TaxRate(
+      ...Model.extractPropsFromJSON(d),
+      extractNumber(d, "location_id"),
+      extractString(d, "name"),
+      extractNumber(d, "rate")
+    );
+  }
+}
