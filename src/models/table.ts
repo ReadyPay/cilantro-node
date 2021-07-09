396313618dc9b29b6ad1502ff2d4a88928ba6315
Derek Milne <derekm@iqmetrix.com>
2021-07-09 11:34:27 -0700

    complete table model

--- src/models/table.ts
+++ src/models/table.ts
@@ -1,12 +1,21 @@
 import { Model } from "./model";
-import { extractNumber, JsonType } from "../json-util";
+import {
+  extractEnum,
+  extractNumber,
+  extractString,
+  JsonType,
+} from "../json-util";
 
 export class Table extends Model {
   constructor(
     readonly id: number,
     readonly createdAt: Date,
     readonly updatedAt: Date,
-    readonly locationId: number
+    readonly locationId: number,
+    readonly name: string,
+    readonly xCoordinate: number,
+    readonly yCoordinate: number,
+    readonly shape: TableShape
   ) {
     super(id, createdAt, updatedAt);
   }
@@ -17,7 +26,16 @@ export class Table extends Model {
     }
     return new Table(
       ...Model.extractPropsFromJSON(d),
-      extractNumber(d, "location_id")
+      extractNumber(d, "location_id"),
+      extractString(d, "name"),
+      extractNumber(d, "x_coordinate"),
+      extractNumber(d, "y_coordinate"),
+      extractEnum(d, "shape", TableShape, TableShape.Square)
     );
   }
+}
+
+export enum TableShape {
+  Rectangle = "rectangle",
+  Square = "square",
 }
