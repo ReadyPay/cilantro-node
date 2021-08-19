28d94a49dda7cf8e86d455d61785bbc08d1e1505
Derek Milne <derekm@iqmetrix.com>
2021-08-19 15:50:57 -0700

    improve payment tender update request

--- src/requests/paymentTender-update.request.ts
+++ src/requests/paymentTender-update.request.ts
@@ -1,17 +1,21 @@
 import { JsonSerializer, JsonType } from "../json-util";
 
+export interface PaymentTenderUpdateFields {
+  name: string;
+}
+
 export class PaymentTenderUpdateRequest implements JsonSerializer {
   constructor(
     readonly id: number,
     readonly locationId: number,
-    readonly name: string
+    readonly fields: PaymentTenderUpdateFields
   ) {}
 
   toJson(): JsonType {
     return {
       id: this.id,
       location_id: this.locationId,
-      name: this.name,
+      name: this.fields.name,
     };
   }
 }
