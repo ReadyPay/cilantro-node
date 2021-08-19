995fda452465b7b9b540bd20e4e0b5c50e97b6f2
Derek Milne <derekm@iqmetrix.com>
2021-08-19 15:48:22 -0700

    improve table update request

--- src/requests/table-update.request.ts
+++ src/requests/table-update.request.ts
@@ -1,25 +1,28 @@
 import { TableShape } from "../models/table";
 import { JsonSerializer, JsonType } from "../json-util";
 
+export interface TableUpdateFields {
+  shape?: TableShape;
+  name?: string;
+  xCoordinate?: number;
+  yCoordinate?: number;
+}
+
 export class TableUpdateRequest implements JsonSerializer {
   constructor(
     readonly id: number,
     readonly locationId: number,
-    readonly shape: TableShape,
-
-    readonly name?: string,
-    readonly xCoordinate?: number,
-    readonly yCoordinate?: number
+    readonly fields: TableUpdateFields
   ) {}
 
   toJson(): JsonType {
     return {
       id: this.id,
       location_id: this.locationId,
-      shape: this.shape,
-      name: this.name,
-      x_coordinate: this.xCoordinate,
-      y_coordinate: this.yCoordinate,
+      shape: this.fields.shape,
+      name: this.fields.name,
+      x_coordinate: this.fields.xCoordinate,
+      y_coordinate: this.fields.yCoordinate,
     };
   }
 }
