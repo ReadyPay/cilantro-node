cfa9624a6281bb32a8be53dc1f9a47bbae958013
Derek Milne <derekm@iqmetrix.com>
2021-08-19 15:49:58 -0700

    improve tax rate update request

--- src/requests/taxRate-update.request.ts
+++ src/requests/taxRate-update.request.ts
@@ -1,20 +1,23 @@
 import { JsonType } from "../json-util";
 
+export interface TaxRateUpdateFields {
+  name?: string;
+  rate?: number;
+}
+
 export class TaxRateUpdateRequest {
   constructor(
     readonly id: number,
     readonly locationId: number,
-
-    readonly name?: string,
-    readonly rate?: number
+    readonly fields: TaxRateUpdateFields
   ) {}
 
   toJson(): JsonType {
     return {
       id: this.id,
       location_id: this.locationId,
-      name: this.name,
-      rate: this.rate,
+      name: this.fields.name,
+      rate: this.fields.rate,
     };
   }
 }
