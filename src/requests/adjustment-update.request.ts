8ddddeb3fda759fdf4ab715d209e6f8a8b25f2a5
Derek Milne <derekm@iqmetrix.com>
2021-08-19 15:49:17 -0700

    improve adjustment update request

--- src/requests/adjustment-update.request.ts
+++ src/requests/adjustment-update.request.ts
@@ -1,17 +1,21 @@
 import { JsonSerializer, JsonType } from "../json-util";
 
+export interface AdjustmentUpdateFields {
+  value: number;
+}
+
 export class AdjustmentUpdateRequest implements JsonSerializer {
   constructor(
     readonly id: number,
     readonly locationId: number,
-    readonly value: number
+    readonly fields: AdjustmentUpdateFields
   ) {}
 
   toJson(): JsonType {
     return {
       id: this.id,
       location_id: this.locationId,
-      value: this.value,
+      value: this.fields.value,
     };
   }
 }
