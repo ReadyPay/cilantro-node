17ee0cedbd172e444dbcf3d1f77312a19a56bbbd
Derek Milne <derekm@iqmetrix.com>
2021-08-20 13:25:45 -0700

    add missing fields to adjustment create and update requests

--- src/requests/adjustment-create.request.ts
+++ src/requests/adjustment-create.request.ts
@@ -1,9 +1,15 @@
 import { JsonType } from "../json-util";
+import { AdjustmentType, CalculationPhase } from "../models/adjustment";
 
 export interface AdjustmentCreateRequest {
   locationId: number;
+  // TODO make these two optional
+  type: AdjustmentType;
+  calculationPhase: CalculationPhase;
 
+  name?: string;
   value?: number;
+  isOpenValue?: boolean;
 }
 
 export function adjustmentCreateRequestToJson(
@@ -11,6 +17,10 @@ export function adjustmentCreateRequestToJson(
 ): JsonType {
   return {
     location_id: r.locationId,
+    type: r.type,
+    calculation_phase: r.calculationPhase,
+    name: r.name,
     value: r.value,
+    is_open_value: r.isOpenValue,
   };
 }
--- src/requests/adjustment-update.request.ts
+++ src/requests/adjustment-update.request.ts
@@ -1,10 +1,15 @@
 import { JsonType } from "../json-util";
+import { AdjustmentType, CalculationPhase } from "../models/adjustment";
 
 export interface AdjustmentUpdateRequest {
   id: number;
   locationId: number;
 
+  type?: AdjustmentType;
+  calculationPhase?: CalculationPhase;
+  name?: string;
   value?: number;
+  isOpenValue?: boolean;
 }
 
 export function adjustmentUpdateRequestToJson(
@@ -13,6 +18,10 @@ export function adjustmentUpdateRequestToJson(
   return {
     id: r.id,
     location_id: r.locationId,
+    type: r.type,
+    calculation_phase: r.calculationPhase,
+    name: r.name,
     value: r.value,
+    is_open_value: r.isOpenValue,
   };
 }
